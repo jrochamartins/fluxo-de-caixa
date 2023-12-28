d3337ae84be15cbdc7d44fbccef35a33038d7468
José Rocha <jrocha.gm@gmail.com>
2023-12-28 01:43:29 -0300

    Validation

--- src/FluxoDeCaixa.Api/ViewModels/PostLoginRequest.cs
+++ src/FluxoDeCaixa.Api/ViewModels/PostLoginRequest.cs
@@ -1,7 +1,10 @@
-﻿namespace FluxoDeCaixa.Api.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FluxoDeCaixa.Api.ViewModels
 {
     public class PostLoginRequest
     {
+        [Required]
         public string? User { get; set; }
     }
 }
